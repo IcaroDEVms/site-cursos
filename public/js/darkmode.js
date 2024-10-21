74f22704ca458f67338a9b2a168d6ee0b895d40e
IcaroDEVms <icaromatossilva@gmail.com>
2024-10-21 16:58:29 -0300

    Mudanças gerais
    alterações no front, alerações no modo escuro, implementação de localstorage para darkmode, correção de bugs

--- public/js/darkmode.js
+++ public/js/darkmode.js
@@ -1,7 +1,18 @@
 const $html = document.querySelector('html')
+// const $checkbox = document.querySelector('#switch')
 
-const $checkbox = document.querySelector('#switch')
 
-$checkbox.addEventListener('change', function(){
-    $html.classList.toggle('dark-mode')
-})
+// $checkbox.addEventListener('change', function(){
+//     $html.classList.toggle('dark-mode')
+// })
+
+window.onload = function(){
+    const darkMode = localStorage.getItem('darkMode');
+
+    if (darkMode === '1') {
+        $html.classList.add('dark-mode');
+        // $checkbox.checked = true; // Ensure the checkbox reflects the current mode
+    } else {
+        localStorage.setItem('darkMode', 0);
+    }
+}
