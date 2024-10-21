20a7ebd397ccd47d8ff9289a15edb53d80a10a13
IsaqueNunes123 <141284556+IsaqueNunes123@users.noreply.github.com>
2024-10-21 19:46:33 -0300

    video funcional

--- public/js/aulas.js
+++ public/js/aulas.js
@@ -1,29 +1,27 @@
 // Função que insere o vídeo no body quando o botão é clicado
-function playVideo(videoUrl) {
-    // Seleciona o container onde o vídeo será injetado
-    const videoContainer = document.getElementById('video-container');
-
-    // Insere o código do vídeo no container
-    videoContainer.innerHTML = `
-        <div class="video-overlay">
-            <div class="video-content">
-                <button onclick="closeVideo()" class="close-video">X</button>
-                <iframe width="560" height="315" src="${videoUrl}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-        </div>
-    `;
-}
-
-// Função para fechar o vídeo e limpar o container
-function closeVideo() {
-    const videoContainer = document.getElementById('video-container');
-    videoContainer.innerHTML = ''; // Limpa o conteúdo
-}
-
-// Detecta clique no botão e chama a função de play do vídeo
-document.querySelectorAll('.accordion-item button').forEach(button => {
-    button.addEventListener('click', function() {
-        const videoUrl = "https://www.youtube.com/embed/dQw4w9WgXcQ"; // URL do vídeo a ser exibido
-        playVideo(videoUrl);
-    });
-});
+        // Função para exibir o vídeo
+        function playVideo(videoUrl) {
+            // Seleciona o container onde o vídeo será injetado
+            const videoContainer = document.getElementById('video-container');
+    
+            // Insere o código do vídeo no container
+            videoContainer.innerHTML = `
+                <div class="video-overlay">
+                    <div class="video-content">
+                        <button onclick="closeVideo()" class="close-video">X</button>
+                            <iframe width="560" height="315" src="${videoUrl}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>                    </div>
+                </div>
+            `;
+        }
+    
+        // Função para fechar o vídeo e limpar o container
+        function closeVideo() {
+            const videoContainer = document.getElementById('video-container');
+            videoContainer.innerHTML = ''; // Limpa o conteúdo
+        }
+    
+        // Adiciona o evento de clique ao botao1
+        document.getElementById('botao1').addEventListener('click', function() {
+            const videoUrl = "https://www.youtube.com/embed/EuTdbZxJzKk?si=NWqqVox8RyBywdcq"; // URL do vídeo a ser exibido
+            playVideo(videoUrl);
+        });
