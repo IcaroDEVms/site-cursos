a85c587ea35f045104c653608f49d7467d2c5e42
Ícaro Matos Silva <145877304+IcaroDEVms@users.noreply.github.com>
2024-10-21 21:34:16 -0300

    bugfix

--- public/js/darkmode.js
+++ public/js/darkmode.js
@@ -6,13 +6,12 @@ const $html = document.querySelector('html')
 //     $html.classList.toggle('dark-mode')
 // })
 
-window.onload = function(){
+window.addEventListener('load', function(){
     const darkMode = localStorage.getItem('darkMode');
 
     if (darkMode === '1') {
         $html.classList.add('dark-mode');
+
         // $checkbox.checked = true; // Ensure the checkbox reflects the current mode
-    } else {
-        localStorage.setItem('darkMode', 0);
     }
-}
+});
